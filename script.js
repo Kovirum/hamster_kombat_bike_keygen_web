7fa4d6daf03d88de4a416372ef8a05e7ecb71e0c
Kovirum <kovirum@mail.ru>
2024-08-17 23:11:48 +0300

    fix(config): correct generator issue
    Updated the configuration to fix an error that occurred in the generator when trying to create a key for the games "MyCloneArmy" and "BikeRide3D"

--- script.js
+++ script.js
@@ -1,29 +1,40 @@
-const EVENTS_DELAY = 20000;
 const defaultLanguage = document.documentElement.getAttribute('lang')
 const gamePromoConfigs = {
     MyCloneArmy: {
         appToken: '74ee0b5b-775e-4bee-974f-63e7f4d5bacb',
-        promoId: 'fe693b26-b342-4159-8808-15e3ff7f8767'
+        promoId: 'fe693b26-b342-4159-8808-15e3ff7f8767',
+        eventsDelay: 70000,
+        attemptsNumber: 11
     },
     ChainCube2048: {
         appToken: 'd1690a07-3780-4068-810f-9b5bbf2931b2',
-        promoId: 'b4170868-cef0-424f-8eb9-be0622e8e8e3'
+        promoId: 'b4170868-cef0-424f-8eb9-be0622e8e8e3',
+        eventsDelay: 20000,
+        attemptsNumber: 10
     },
     TrainMiner: {
         appToken: '82647f43-3f87-402d-88dd-09a90025313f',
-        promoId: 'c4480ac7-e178-4973-8061-9ed5b2e17954'
+        promoId: 'c4480ac7-e178-4973-8061-9ed5b2e17954',
+        eventsDelay: 20000,
+        attemptsNumber: 10
     },
     BikeRide3D: {
         appToken: 'd28721be-fd2d-4b45-869e-9f253b554e50',
-        promoId: '43e35910-c168-4634-ad4f-52fd764a843f'
+        promoId: '43e35910-c168-4634-ad4f-52fd764a843f',
+        eventsDelay: 20000,
+        attemptsNumber: 22
     },
     MergeAway: {
         appToken: '8d1cc2ad-e097-4b86-90ef-7a27e19fb833',
         promoId: 'dc128d28-c45b-411c-98ff-ac7726fbaea4',
+        eventsDelay: 20000,
+        attemptsNumber: 10
     },
     TwerkRace: {
         appToken: '61308365-9d16-4040-8bb0-2f4a4c69074c',
-        promoId: '61308365-9d16-4040-8bb0-2f4a4c69074c'
+        promoId: '61308365-9d16-4040-8bb0-2f4a4c69074c',
+        eventsDelay: 20000,
+        attemptsNumber: 10
     }, 
 };
 
@@ -157,11 +168,11 @@ document.getElementById('startBtn').addEventListener('click', async () => {
             startBtn.disabled = false;
             return null;
         }
-
-        for (let i = 0; i < 10; i++) {
-            await sleep(EVENTS_DELAY * delayRandom());
+        
+        for (let i = 0; i < currentAppConfig.attemptsNumber; i++) {
+            await sleep(currentAppConfig.eventsDelay * delayRandom());
             const hasCode = await emulateProgress(clientToken);
-            updateProgress(10 / keyCount);
+            updateProgress((100 / currentAppConfig.attemptsNumber) / keyCount);
             if (hasCode) {
                 break;
             }
