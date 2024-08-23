76966c0d48645eb9f844ac5a05ce901b526ff1df
Kovirum <98853474+Kovirum@users.noreply.github.com>
2024-08-23 12:43:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -45,13 +45,13 @@ const gamePromoConfigs = {
     MowandTrim: {
         appToken: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
         promoId: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
-        eventsDelay: 20,
+        eventsDelay: 20000,
         attemptsNumber: 20
     },
     MudRacing: {
         appToken: '8814a785-97fb-4177-9193-ca4180ff9da8',
         promoId: '8814a785-97fb-4177-9193-ca4180ff9da8',
-        eventsDelay: 20,
+        eventsDelay: 20000,
         attemptsNumber: 20
     }
 };
